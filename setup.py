c9edbd446fcf0a9af72bf5113e9b1c39d2fe25d7
chriscohoat <chris.cohoat@gmail.com>
2011-08-26 15:16:55 -0400

    Changed install name for this distribution.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='highrisewrapper',
+    name='highrise',
     version='0.1.0',
     author='Chris Cohoat',
     author_email='chris.cohoat@gmail.com',
-    packages=['highrisewrapper', 'highrisewrapper.test'],
-    url='https://github.com/chriscohoat/highrisewrapper',
+    packages=['highrise', 'highrise.test'],
+    url='https://github.com/chriscohoat/highrise',
     license='LICENSE.txt',
     description='Pythonic wrapper for Highrise.',
     long_description=open('README.txt').read(),
